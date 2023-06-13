8b362da6f365f411d9aa0468989149bb7133b5ab
yang-mingzhou <yang7492@umn.edu>
2023-06-13 15:08:03 -0500

    first version

--- src_cpp/graph.hpp
+++
@@ -1,7 +0,0 @@
-// graph.hpp
-
-#include <vector>
-
-extern std::vector<std::vector<unsigned>> adjacent_table;
-extern std::vector<std::vector<unsigned>> pred_adjacent_table;
-extern std::vector<gnode> graph_node;
